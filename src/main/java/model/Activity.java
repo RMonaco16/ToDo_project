07620f3c0fda239ef77b4d32ec3dc908b65f367c
ClaudioUni03 <clau.manganiello@studenti.unina.it>
2025-04-17 16:24:21 +0200

    provaprova

---
+++ src/main/java/model/Activity.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.Date;
+
+public class Activity {
+    private String name;
+    private boolean state;
+    private Date dataCompletamento;
+}
---
+++ src/main/java/model/ToDo.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ToDo {
+    private String title;
+    private String description;
+    //private String colore;
+    private int position;
+    //private immagine
+    //private url
+    private boolean state;
+}
