4cf066a5bc4e9a7aba3814e16220203205ba3c59
ClaudioUni03 <clau.manganiello@studenti.unina.it>
2025-06-30 15:34:33 +0200

    remove no usage

--- src/main/java/gui/Register.java
+++ src/main/java/gui/Register.java
@@ -223,6 +223,4 @@ public class Register {
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
     }
-
-
 }
--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -43,34 +43,6 @@ public class Board {
         this.toDo = toDo;
     }
 
-    public void searchToDoAddActivity(String titleToDo, Activity a) {
-        int trovato = 0;
-        for (int i = 0; i < this.toDo.size(); i++) {
-            if (titleToDo.equalsIgnoreCase(this.toDo.get(i).getTitle())) {
-                trovato = 1;
-                this.toDo.get(i).getCheckList().addActivity(a);
-                this.toDo.get(i).setState(false);
-            }
-        }
-        if (trovato == 0) {
-            System.out.println("ToDo non trovato..");
-        }
-    }
-
-
-    public void searchToDoRemoveActivity(String titleToDo, String nameActivity){
-        int trovato = 0;
-        for(int i = 0; i < this.toDo.size(); i++){
-            if(titleToDo.equalsIgnoreCase(this.toDo.get(i).getTitle())){
-                trovato = 1;
-                this.toDo.get(i).getCheckList().rmvActivity(nameActivity);
-            }
-        }
-        if(trovato == 0){
-            System.out.println("ToDo non trovato..");
-        }
-    }
-
     public boolean boardAddToDo(ToDo todo){
         boolean nuova = true;
         for(int i = 0; i < this.toDo.size(); i++){
@@ -87,24 +59,6 @@ public class Board {
         return nuova; // verso se aggiunta
     }
 
-    public void srcTodocheck(String nmTodo,String nmAct, String dataCompletamento){
-        for(ToDo t : toDo){
-            if(nmTodo.equalsIgnoreCase(t.getTitle())){
-                t.getCheckList().checkAct(nmAct, dataCompletamento);
-                return;
-            }
-        }
-    }
-
-    public void srcTodoDeCheck(String nmTodo,String nmAct){
-        for(ToDo t : toDo){
-            if(nmTodo.equalsIgnoreCase(t.getTitle())){
-                t.getCheckList().deCheckAct(nmAct);
-                t.setState(false);
-                return;
-            }
-        }
-    }
     // ritorna vero se trova un to-do già esistente con lo stesso nome
     public boolean checkNameToDoAlreadyExisting(String nameToDo){
         for(ToDo t: toDo){
@@ -112,81 +66,5 @@ public class Board {
                 return true;
         }
         return false;
-    }
-
-
-
-    public boolean srcToDoToEdit(String ToDoToSrc,String newNameToDo,String description,LocalDate expiration,String image, Color color) {
-        boolean result = false;
-        for (ToDo t : toDo) {
-            if (ToDoToSrc.equalsIgnoreCase(t.getTitle())) {
-                if(!newNameToDo.equals(ToDoToSrc))
-                    result = checkNameToDoAlreadyExisting(newNameToDo);
-                if (!result)
-                    t.setTitle(newNameToDo);
-
-                t.setDescription(description);
-
-                if (expiration != null) {
-                    t.setExpiration(expiration);
-                } else {
-                    t.setExpiration(null);
-                }
-                t.setImage(image);
-                t.setColor(color);
-            }
-        }
-        return result;
-    }
-
-
-    public void srcToDoifComplete(String nmTodo,ArrayList<ToDo> visibleToDo) {
-        for (ToDo t : visibleToDo) {
-            if (nmTodo.equalsIgnoreCase(t.getTitle())) {
-                if (t.getCheckList().checkToDoComplete() && !t.getCheckList().getActivities().isEmpty()) {
-                    t.setState(true);
-                    System.out.println("stateTodo = true");
-                } else {
-                    t.setState(false);
-                }
-                return;
-            }
-            return;
-        }
-    }
-
-    public ArrayList<ToDo> print(){
-        ArrayList<ToDo> todoList = new ArrayList<>();
-        for(ToDo t : toDo){
-            todoList.add(t);
-        }
-        return todoList;
-    }
-
-    public ArrayList<Activity> srcTodoPrint(String nmTodo){
-        ArrayList<Activity> activities = new ArrayList<>();
-        for(ToDo t : toDo){
-            if(nmTodo.equalsIgnoreCase(t.getTitle())){
-                return t.getCheckList().getActivities();
-            }
-        }
-        return activities;
-    }
-
-    public void srcTodoSwap(String nmTodo,int i){
-        for(ToDo t: toDo){
-            if(t.getTitle().equalsIgnoreCase(nmTodo)){
-                Collections.swap(toDo,toDo.indexOf(t.getTitle()), i);
-            }
-        }
-    }
-
-    public void printRange(LocalDate range){
-        for(ToDo t: toDo){
-            //long giorniRimanenti = ChronoUnit.DAYS.between(oggi, dataScadenza);
-            if(t.getExpiration().compareTo(range)>=0){
-                System.out.println(t.getTitle()+" | ");//+ t.getDescription());
-            }
-        }
     }
 }
